043d7b64f5c2ce86c7220a4619cbc179ddff52da
Alex Howard <thezanke@gmail.com>
2018-05-09 19:41:07 -0400

    converted flipCoin to hubot syntax

--- messages/impossible.js
+++ messages/impossible.js
@@ -2,7 +2,7 @@ module.exports = user => {
   const message = ["I'm afraid I can't do that"];
 
   if (user) {
-    message.push(`, <@${user}>.`);
+    message.push(`, <@${user.id}>.`);
   } else {
     message.push('.');
   }
---
+++ scripts/flipCoin.js
@@ -0,0 +1,94 @@
+const crypto = require('crypto');
+const messages = require('../messages');
+
+module.exports = robot => {
+  robot.hear(/!flip(?: ([0-9]*))?$/i, res => {
+    const { user } = res.message;
+    const [, _flips = 1] = res.match;
+    const flips = parseInt(_flips, 10);
+
+    if (flips > 100) {
+      res.send(messages.impossible(user));
+      return;
+    }
+
+    const details = {
+      heads: 0,
+      tails: 0,
+      sequences: []
+    };
+
+    let currentSequence = { face: null, length: 0 };
+
+    const rng = crypto.randomBytes(flips);
+    const results = Array.from(rng, number => {
+      const face = number % 2 ? 'heads' : 'tails';
+
+      if (flips > 10) {
+        details[face] += 1;
+
+        if (face !== currentSequence.face) {
+          if (currentSequence.length >= 2)
+            details.sequences.push(currentSequence);
+          currentSequence = { face, length: 1 };
+        } else {
+          currentSequence.length += 1;
+        }
+      }
+
+      return `:coin-${face}:`;
+    });
+
+    const plural = (count, s, p) => (count > 1 ? p : s);
+
+    const reply = [results.join(' ')];
+    const timestamp = Math.floor(Date.now() / 1000);
+
+    const attachment = {
+      fallback: `Results for ${flips} Coin ${plural(
+        flips,
+        'Flip',
+        'Flips'
+      )} - Heads: ${details.heads} - Tails: ${details.tails}`,
+      author_name: 'Coin Flipper',
+      title: `Results for ${flips} Coin ${plural(flips, 'Flip', 'Flips')}`,
+      pretext: `*<@${user.id}> flipped ${plural(
+        flips,
+        'a coin',
+        `${flips} coins`
+      )}*`,
+      text: reply.join(''),
+      ts: timestamp,
+      fields: [],
+      mrkdwn_in: ['pretext']
+    };
+
+    if (flips > 10) {
+      const sequence = details.sequences.sort((a, b) => {
+        if (b.length > a.length) return 1;
+        if (b.length < a.length) return -1;
+        return 0;
+      })[0];
+
+      attachment.fields.push(
+        {
+          title: 'Total Heads',
+          value: `${details.heads} × :coin-heads:`,
+          short: true
+        },
+        {
+          title: 'Total Tails',
+          value: `${details.tails} × :coin-tails:`,
+          short: true
+        },
+        {
+          title: 'Longest Sequence',
+          value: `${sequence.length} × :coin-${sequence.face}: in a row!`,
+          short: true
+        }
+      );
+    }
+
+    res.reply({ attachments: [attachment] });
+  });
+};
