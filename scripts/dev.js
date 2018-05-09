db193266d748119c2ae131099997e8487d16aec8
Alex Howard <thezanke@gmail.com>
2018-05-09 18:32:36 -0400

    adding dev scripts

---
+++ scripts/dev.js
@@ -0,0 +1,9 @@
+module.exports = robot => {
+  robot.hear(/channel id/i, res => {
+    res.reply(res.envelope.room);
+  });
+
+  robot.hear(/my id/i, res => {
+    res.reply(res.envelope.user.id);
+  });
+}
