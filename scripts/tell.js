e4d3eb01f60d68ad60a0a4c8c2623b440a4b5b08
Alex Howard <thezanke@gmail.com>
2018-05-09 17:13:00 -0400

    remove extra code

--- scripts/tell.js
+++ scripts/tell.js
@@ -1,10 +1,7 @@
 const matchers = require('./helpers/matchers');
 
-module.exports = robot => {
-  // const room = 'giantbots';
-
+module.exports = robot =>
   robot.hear(new RegExp(`tell ([@#]?${matchers.name}) (.*)`, 'i'), res => {
     const [,nameOrChannel, message] = res.match;
     robot.messageRoom(nameOrChannel, message);
   });
-}
